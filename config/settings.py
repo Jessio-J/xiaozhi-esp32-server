88483480c0637e2155e58e598f2741c18b6d8d89
jiangzhou <jiangzhou1@xiaohongshu.com>
2025-02-11 14:56:22 +0800

    config-self

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,6 @@ from core.utils.util import read_config
 def load_config():
     """加载配置文件"""
     parser = argparse.ArgumentParser(description="Server configuration")
-    parser.add_argument("--config_path", type=str, default="config.yaml")
+    parser.add_argument("--config_path", type=str, default="config-self.yaml")
     args = parser.parse_args()
     return read_config(args.config_path)
